fb75c5a504a8fbf2cd62af43ffa363b0b0a86ff6
CubE135 <gitlab@cub3mail.de>
2022-09-23 11:51:53 +0200

    ✨ Initial App

---
+++ components/NerdButton.vue
@@ -0,0 +1,43 @@
+<template>
+    <button class="bg-gray-300 hover:bg-gray-400 rounded-md shadow" :class="style + margin">{{ text }}</button>
+</template>
+
+<script>
+export default {
+    props: {
+        text: {
+            type: String,
+            required: true
+        },
+        size: {
+            type: String,
+            default: 'sm',
+            required: false
+        },
+        margin: {
+            type: String,
+            default: 'm-1',
+            required: false
+        },
+    },
+    data() {
+        return {}
+    },
+    computed: {
+        style() {
+            switch (this.size) {
+                case 'sm':
+                    return 'py-1 px-2 text-sm '
+                case 'md':
+                    return 'py-1 px-4 text-lg '
+                case 'lg':
+                    return 'py-3 px-6 text-xl '
+                case 'xl':
+                    return 'py-3 px-10 text-3xl '
+                default:
+                    return 'py-4 px-8'
+            }
+        },
+    }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="relative flex items-top justify-center min-h-screen bg-gray-100 sm:items-center sm:pt-0">
+    <div class="max-w-4xl mx-auto sm:px-6 lg:px-8">
+      <img src="~/assets/img/logo.png" class="mb-4" alt>
+      <div class="flex justify-evenly">
+        <!-- <NerdButton text="Erstellen" size="sm" /> -->
+        <!-- <NerdButton text="Erstellen" size="md" /> -->
+        <NerdButton text="Erstellen" size="lg" />
+        <!-- <NerdButton text="Erstellen" size="xl" /> -->
+        <NerdButton text="Beitreten" size="lg" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style scoped>
+img {
+  width: 250px;
+}
+</style>
